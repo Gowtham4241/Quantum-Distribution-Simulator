3a83e83ecc21bad85facd99ca2195a5a0cfc6c5a
Gowtham4241 <23501a4241@pvpsit.ac.in>
2026-02-05 21:58:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ try:
     # QiskitRuntimeService.save_account(channel="ibm_quantum", token="YOUR_TOKEN", overwrite=True)
    service = QiskitRuntimeService(
     channel="ibm_quantum",
-     instance="crn:v1:bluemix:public:quantum-computing:us-east:a/0dabbbeda08947b288425684a966111a:d7d35d82-ad79-4cc1-9169-215eb581e1ef::",
+    instance="crn:v1:bluemix:public:quantum-computing:us-east:a/0dabbbeda08947b288425684a966111a:d7d35d82-ad79-4cc1-9169-215eb581e1ef::",
     token=os.environ.get("IBM_QUANTUM_TOKEN")
 )
     
@@ -366,3 +366,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render will supply PORT
 
     app.run(host="0.0.0.0", port=port, debug=False,use_reloader=False)
+
